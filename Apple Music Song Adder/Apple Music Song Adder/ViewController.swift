fc223b4749d655555ff234572065f45558d69125
Joe <z324liu@uwaterloo.ca>
2016-11-13 00:54:58 -0500

    first commit

---
+++ Apple Music Song Adder/Apple Music Song Adder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Apple Music Song Adder
+//
+//  Created by Joe Liu on 2016-11-13.
+//  Copyright © 2016 Joe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
