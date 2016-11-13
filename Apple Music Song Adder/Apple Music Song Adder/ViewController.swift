61f367c1d72969b6263c13fe3498eb030a1993d3
Joe <z324liu@uwaterloo.ca>
2016-11-13 02:43:00 -0500

    request apple music usage authorization

--- Apple Music Song Adder/Apple Music Song Adder/ViewController.swift
+++ Apple Music Song Adder/Apple Music Song Adder/ViewController.swift
@@ -7,19 +7,35 @@
 //
 
 import UIKit
+import StoreKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var testLabel: UILabel!
+    var controller: SKCloudServiceController!
+    var status: SKCloudServiceAuthorizationStatus!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        SKCloudServiceController.requestAuthorization { (status) in
+            // create the alert
+            let alert = UIAlertController(title: String(status.rawValue), message: String(status.rawValue), preferredStyle: UIAlertControllerStyle.alert)
+            
+            // add an action (button)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+            
+            // show the alert
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
